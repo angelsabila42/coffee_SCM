c09b1429714d139b9ab2548b1d50a4dfd1bdc5c3
Tabitha Chebet <tabithasabila48@gmail.com>
2025-06-14 17:58:09 +0300

    Installed Laravel Livewire, modified the dashboard slightly

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,9 +1,7 @@
-@props(['title'=>'Untitled'])
-
  <div class = "row mb-4">
         <div class="col-md-12">
-            <div class="mb-0 font-weight-bold pl-3">
-                <h3> {{$title}} </h3>
+            <div class="font-weight-bold pl-3">
+                <h3> {{$slot}} </h3>
             </div>
         </div>
     </div>
