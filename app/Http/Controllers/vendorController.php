6752c859f959193280e0f6408a41cb72a6a2fe84
IAM-NO-4 <muwayirei@gmail.com>
2025-06-22 16:50:35 +0300

    breeze

--- app/Http/Controllers/vendorController.php
+++ app/Http/Controllers/vendorController.php
@@ -5,33 +5,59 @@ use App\Models\User;
 use App\Models\vendor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Http;
 class vendorController extends Controller
 {
 
-    
-//   'name',
-//         'email',
-//        'email',
-//         'phone_number',
-//         'street',
-//         'city',
+   public function pdfValidation(Request $request){
+     $pdf =$request->file('document');
+      $pdf= base64_encode(file_get_contents($pdf->getRealPath()
+    ));
+   $res = Http::Post('http://localhost:8081/api/verify',[
+   
+    'pdf' => $pdf,
+   ]);
+   return $res->body();
+}
+
+  public function register(Request $request){
+     $pdf =$request->file('document');
+      $pdf= base64_encode(file_get_contents($pdf->getRealPath()
+    ));
+
+   $res = Http::Post('http://localhost:8081/api/verify',[
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'password' => $request->input('password'),
+        'confpassword' => $request->input('password_confirmation'),
+
+    'pdf' => $pdf,
+   ]);
+   return $res->body();
+  
+
+    # code...
+   }
+
     public function store(Request $req){
 
-    $validated = $req->validate([
-        'name' => 'required',
+
+      $validated = $req->validate([
+         'name' => 'required',
         'email' => 'required|email|unique:vendor,email',
         'password' => ['required','confirmed','min:8'],
         'street' => '',
          'city' => '',
         'phone_number' => 'required|regex:/^07[0-9]{8}$/',
-         'document' => 'required|file|mimes:pdf',
+       // 'document' => 'required|file|mimes:pdf',
         
 
-    ]);   
-     $validated['password'] = Hash::make($validated['password']);
+   ]);   
+   $validated['password'] = Hash::make($validated['password']);
      $filepath = $req->file('document')->store('vendor_files','public');
     
+
+
     vendor::create($validated);
      $fields = collect($validated)->only([
         'name','email','password'
@@ -39,5 +65,12 @@ class vendorController extends Controller
 
     User::create($fields);
     return redirect()->back();
+
     }
 }
+
+   
+    
+    
+    
+  
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,24 @@
 <?php
-
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AnalyticsController;
-use App\Http\Controllers\authController;
-use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\vendorController;
 use App\Http\Controllers\staffController;
+use App\Http\Controllers\authController;
 use App\Http\Controllers\transporterController;
-use App\Http\Controllers\ImporterModelController;
-use App\Models\transporter;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AnalyticsController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ReportController;
-use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImporterModelController;
+use Illuminate\Validation\Rules\Email;
 
-
-
-
-Route::get('/', function () {
-    return view('auth.login');
-});
-
-Auth::routes();
+Route::get('/vendor', function () {
+    return view('auth.vendor');
+})->name('vendor');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
@@ -39,28 +33,24 @@ Route::resource('payments', PaymentController::class);
 Route::get('/order', function () {
     return view('order');
 });
-
 Route::get('/inventory', function () {
     return view('inventory');
 });
-Route::get('/form_modal', function () {
-    return view('form_modal');
+
+Route::get('/dashboard', function () {
+    return view('analytics');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+require __DIR__.'/auth.php';
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/home/dashboard', function(){
-    return view('home');
-});
-
-
-
-// auth routes...
+// custom auth routes
+Route::post('/reg/vendor', [vendorController::class, 'store'])->name('store.vendor');
 Route::get('/reg', [authController::class, 'category'])->name('category');
 Route::get('/reg/vendor', [authController::class, 'vendor'])->name('vendor');
 Route::get('/reg/staff', [staffController::class, 'staff'])->name('staff');
@@ -69,8 +59,11 @@ Route::get('/reg/others', [authController::class, 'others'])->name('others');
 Route::get('/reg/importer', [ImporterModelController::class, 'importer'])->name('importer');
 Route::post('/reg', [authController::class, 'handleSelection'])->name('select.category');
 
-//registration routes
 Route::post('/reg/vendor', [vendorController::class, 'store'])->name('store.vendor');
 Route::post('/reg/staff', [staffController::class, 'store'])->name('store.staff');
 Route::post('/reg/transporter', [transporterController::class, 'store'])->name('store.transporter');
 Route::post('/reg/importer', [ImporterModelController::class, 'store'])->name('store.importer');
+
+
+Route::get("/java",[vendorController::class, 'store'])-> name('java');
+Route::post("/java",[vendorController::class, 'register'])-> name('java.store');
