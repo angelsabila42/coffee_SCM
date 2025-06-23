c91704d2c2f347564e119bee3793831e0c54928d
lunkss40 <lunkies40@gmail.com>
2025-06-23 13:33:24 +0300

    staff model
     added staff.php

--- app/Models/staff.php
+++ app/Models/staff.php
@@ -3,20 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class staff extends Model
+class Staff extends Model
 {
-    
+     use HasFactory;
 
+    /**
+     * The attributes are mass assignable.
+     */
     protected $fillable = [
-
-        'name',
-        'email',
-        'phone_number',
+        'full_name',
+        'work_center',
         'role',
         'status',
-        'password',
-        'confirm password',
+        'phone_number',
+        'email',
+       
+        ];
+         //  relationships for WorkAssignment and Leave
+    public function workAssignments()
+    {       // // A Staff member has many WorkAssignments
+        return $this->hasMany(WorkAssignment::class);
+    }
 
-    ];
+    public function leaveHistory()
+    {
+        return $this->hasMany(LeaveHistory::class);
+    }
 }
